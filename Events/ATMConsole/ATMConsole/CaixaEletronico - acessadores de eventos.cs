38b8709bde4879f75b490e32f7df9a421eb4afe6
marcelooliveira <mclricardo@gmail.com>
2024-08-20 22:24:12 -0300

    ATMConsole_events

---
+++ Events/ATMConsole/ATMConsole/CaixaEletronico - acessadores de eventos.cs
@@ -0,0 +1,118 @@
+﻿public delegate void ConsultaBancaria();
+public delegate void TransacaoBancaria(decimal amount);
+//public delegate void SaldoInsuficienteHandler();
+
+public class SaldoInsuficienteEventArgs : EventArgs
+{
+    public decimal Saldo { get; }
+    public decimal Saque { get; }
+
+    public SaldoInsuficienteEventArgs(decimal saldo, decimal saque)
+    {
+        Saldo = saldo;
+        Saque = saque;
+    }
+}
+
+public delegate void SaldoInsuficienteHandler(object sender, SaldoInsuficienteEventArgs e);
+
+public class CaixaEletronico : ICaixaEletronico
+{
+    private decimal saldo;
+    private List<string> transacoes = new List<string>();
+    // Evento para saldo insuficiente
+    //public event SaldoInsuficienteHandler SaldoInsuficiente;
+
+    //Enfatizar que o EventHandler abaixo é um delegate:
+    //      public delegate void EventHandler(object? sender, EventArgs e);
+
+    //event EventHandler SaldoInsuficienteEvent;
+    event SaldoInsuficienteHandler SaldoInsuficienteEvent;
+
+
+    object objectLock = new Object();
+
+    public CaixaEletronico()
+    {
+        saldo = 0;
+    }
+
+    public void VerificarSaldo()
+    {
+        Console.WriteLine("*** VerificarSaldo");
+        Console.WriteLine($"Saldo atual: {saldo:C}");
+        Console.WriteLine();
+    }
+
+    public void Extrato()
+    {
+        Console.WriteLine("*** Extrato");
+        foreach (var item in transacoes) { Console.WriteLine(item); }
+        Console.WriteLine();
+    }
+
+    public void Depositar(decimal valor)
+    {
+        Console.WriteLine("*** Depositar");
+        saldo += valor;
+        string itemExtrato = $"Valor depositado: {valor:C}. Novo saldo: {saldo:C}";
+        transacoes.Add(itemExtrato);
+        Console.WriteLine(itemExtrato);
+        Console.WriteLine();
+    }
+
+    public void Sacar(decimal valor)
+    {
+        Console.WriteLine("*** Sacar");
+
+        if (valor > saldo)
+        {
+            //Console.WriteLine("Saldo insuficiente.");
+            // Dispara o evento se o saldo for insuficiente
+            //SaldoInsuficiente?.Invoke();
+            //SaldoInsuficienteEvent?.Invoke(this, EventArgs.Empty);
+            SaldoInsuficienteEvent?.Invoke(this, new SaldoInsuficienteEventArgs(saldo, valor));
+        }
+        else
+        {
+            saldo -= valor;
+            string itemExtrato = $"Valor do saque: {valor:C}. Novo saldo: {saldo:C}";
+            transacoes.Add(itemExtrato);
+            Console.WriteLine(itemExtrato);
+        }
+        Console.WriteLine();
+    }
+
+    //// Método para assinar o evento
+    //public void AssinarSaldoInsuficiente()
+    //{
+    //    SaldoInsuficiente += InformarSaldoInsuficiente;
+    //}
+
+    //// Método para cancelar a assinatura do evento
+    //public void CancelarAssinatura()
+    //{
+    //    SaldoInsuficiente -= InformarSaldoInsuficiente;
+    //}
+
+    // Explicit interface implementation required.
+    // Associate IShape's event with
+    // PostDrawEvent
+    event SaldoInsuficienteHandler ICaixaEletronico.OnSaldoInsuficiente
+    {
+        add
+        {
+            lock (objectLock)
+            {
+                SaldoInsuficienteEvent += value;
+            }
+        }
+        remove
+        {
+            lock (objectLock)
+            {
+                SaldoInsuficienteEvent -= value;
+            }
+        }
+    }
+}
---
+++ Events/ATMConsole/ATMConsole/Program - acessadores de eventos.cs
@@ -0,0 +1,61 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        ICaixaEletronico atm = new CaixaEletronico();
+
+        TransacaoBancaria transacao;
+        ConsultaBancaria consulta;
+
+        consulta = atm.VerificarSaldo;
+        consulta();
+
+        transacao = atm.Depositar;
+        transacao(100);
+        transacao(40);
+        transacao(25);
+
+        transacao = atm.Sacar;
+        transacao(50);
+        transacao(20);
+
+        // Assinando o evento
+        //atm.AssinarSaldoInsuficiente();
+        atm.OnSaldoInsuficiente += Atm_OnSaldoInsuficiente; ;
+        transacao(500);
+
+        // Cancelando a assinatura do evento
+        //atm.CancelarAssinatura();
+        atm.OnSaldoInsuficiente -= Atm_OnSaldoInsuficiente;
+        transacao(500);
+
+        consulta = atm.Extrato;
+        consulta();
+
+        Console.WriteLine("Multicast delegate");
+        Console.WriteLine("==================");
+
+        ConsultaBancaria saldo, extrato, multicastDelegate, outroMulticast;
+        saldo = atm.VerificarSaldo;
+        extrato = atm.Extrato;
+        multicastDelegate = saldo + extrato;
+        multicastDelegate();
+
+        Console.WriteLine("Multicast sem o saldo");
+        Console.WriteLine("=====================");
+
+        outroMulticast = multicastDelegate - saldo;
+        outroMulticast();
+    }
+
+    //private static void Atm_OnSaldoInsuficiente(object sender, EventArgs e)
+    //{
+    //    Console.WriteLine("Saldo insuficiente. Contate a central de atendimento do banco ByteBank para solicitar um limite maior no crédito para emergências.");
+    //}
+
+    private static void Atm_OnSaldoInsuficiente(object sender, SaldoInsuficienteEventArgs e)
+    {
+        Console.WriteLine($"Saldo insuficiente ({e.Saldo:C}) para o valor do saque ({e.Saque:C}). Contate a central de atendimento do banco ByteBank para solicitar um limite maior no crédito para emergências.");
+    }
+
+}
