5007f36cab7f782c08827f65f41e91faf8beffae
marcelooliveira <mclricardo@gmail.com>
2024-08-21 23:20:58 -0300

    Comparando Expressões Lambda com Delegates / Lambdas de expressão e Lambdas de instrução

---
+++ Lambda/ATMConsole/ATMConsole/ICaixaEletronico.cs
@@ -0,0 +1,9 @@
+﻿public interface ICaixaEletronico
+{
+    void Depositar(decimal valor);
+    void Extrato();
+    void Sacar(decimal valor);
+    void VerificarSaldo();
+
+    event SaldoInsuficienteHandler OnSaldoInsuficiente;
+}
