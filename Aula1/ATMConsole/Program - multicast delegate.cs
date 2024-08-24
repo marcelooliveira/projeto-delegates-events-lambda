90c5828ddbbdaaddc05115e4316f3b50c13cdea5
marcelooliveira <mclricardo@gmail.com>
2024-08-24 16:09:28 -0300

    Aula 1 - primeiro commit

---
+++ Aula1/ATMConsole/Program - multicast delegate.cs
@@ -0,0 +1,40 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        CaixaEletronico atm = new CaixaEletronico();
+
+        TransacaoBancaria transacao;
+        ConsultaBancaria consulta;
+
+        consulta = atm.VerificarSaldo;
+        consulta();
+
+        transacao = atm.Depositar;
+        transacao(100);
+        transacao(40);
+        transacao(25);
+
+        transacao = atm.Sacar;
+        transacao(50);
+        transacao(20);
+
+        consulta = atm.Extrato;
+        consulta();
+
+        Console.WriteLine("Multicast delegate");
+        Console.WriteLine("==================");
+
+        ConsultaBancaria saldo, extrato, multicastDelegate, outroMulticast;
+        saldo = atm.VerificarSaldo;
+        extrato = atm.Extrato;
+        multicastDelegate = saldo + extrato;
+        multicastDelegate();
+
+        Console.WriteLine("Multicast sem o saldo");
+        Console.WriteLine("=====================");
+
+        outroMulticast = multicastDelegate - saldo;
+        outroMulticast();
+    }
+}
