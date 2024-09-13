5a97c6dcff5476101d82825b6ebdabcdec1b635b
marcelooliveira <mclricardo@gmail.com>
2024-09-13 00:18:48 -0300

    aula1 antes

---
+++ Aula1/antes/ByteBank.CaixaEletronico/CaixaEletronico.cs
@@ -0,0 +1,54 @@
+﻿public delegate void ConsultaBancaria();
+public delegate void TransacaoBancaria(decimal amount);
+public class CaixaEletronico
+{
+    private decimal saldo;
+    private List<string> transacoes = new List<string>();
+
+    public CaixaEletronico()
+    {
+        saldo = 0;
+    }
+
+    public void VerificarSaldo()
+    {
+        Console.WriteLine("*** VerificarSaldo");
+        Console.WriteLine($"Saldo atual: {saldo:C}");
+        Console.WriteLine();
+    }
+
+    public void Extrato()
+    {
+        Console.WriteLine("*** Extrato");
+        foreach (var item in transacoes) { Console.WriteLine(item); }
+        Console.WriteLine();
+    }
+
+    public void Depositar(decimal valor)
+    {
+        Console.WriteLine("*** Depositar");
+        saldo += valor;
+        string itemExtrato = $"Valor depositado: {valor:C}. Novo saldo: {saldo:C}";
+        transacoes.Add(itemExtrato);
+        Console.WriteLine(itemExtrato);
+        Console.WriteLine();
+    }
+
+    public void Sacar(decimal valor)
+    {
+        Console.WriteLine("*** Sacar");
+
+        if (valor > saldo)
+        {
+            Console.WriteLine("Saldo insuficiente.");
+        }
+        else
+        {
+            saldo -= valor;
+            string itemExtrato = $"Valor do saque: {valor:C}. Novo saldo: {saldo:C}";
+            transacoes.Add(itemExtrato);
+            Console.WriteLine(itemExtrato);
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Aula1/antes/ByteBank.CaixaEletronico/Program.cs
@@ -0,0 +1,84 @@
+﻿using System.Reflection;
+
+internal class Program
+{
+    static CaixaEletronico atm = new CaixaEletronico();
+    static TransacaoBancaria transacao;
+    static ConsultaBancaria consulta;
+
+    private static void Main(string[] args)
+    {
+        
+        MostrarBanner();
+
+        while (true)
+        {
+            MostrarMenu();
+
+            if (int.TryParse(Console.ReadLine(), out int escolha))
+            {
+                ExecutarEscolha(escolha);
+            }
+            else
+            {
+                Console.WriteLine("Opção inválida. Tente novamente.");
+            }
+        }
+
+        static void MostrarBanner()
+        {
+            Console.WriteLine(@"
+
+
+    ____        __       ____              __      
+   / __ )__  __/ /____  / __ )____ _____  / /__    
+  / __  / / / / __/ _ \/ __  / __ `/ __ \/ //_/    
+ / /_/ / /_/ / /_/  __/ /_/ / /_/ / / / / ,<       
+/_____/\__, /\__/\___/_____/\__,_/_/ /_/_/|_|      
+      /____/                                       
+                                
+        ");
+        }
+
+        static void MostrarMenu()
+        {
+            Console.WriteLine("\nEscolha uma opção:");
+            Console.WriteLine();
+            Console.WriteLine("1. Rodar operações");
+            Console.WriteLine();
+            Console.Write("Digite o número da opção desejada: ");
+        }
+
+        static void ExecutarEscolha(int escolha)
+        {
+            switch (escolha)
+            {
+                case 1:
+                    RodarOperacoes();
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida. Tente novamente.");
+                    break;
+            }
+        }
+
+        static void RodarOperacoes()
+        {
+            consulta = atm.VerificarSaldo;
+            consulta();
+
+            transacao = atm.Depositar;
+            transacao(100);
+            transacao(40);
+            transacao(25);
+
+            transacao = atm.Sacar;
+            transacao(50);
+            transacao(20);
+
+            consulta = atm.Extrato;
+            consulta();
+        }
+    }
+}
