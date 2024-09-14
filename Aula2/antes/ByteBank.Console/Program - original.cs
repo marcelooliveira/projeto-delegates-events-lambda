658b5f0c46ad12b02af3d8722e6001d194a10381
marcelooliveira <mclricardo@gmail.com>
2024-09-14 18:23:21 -0300

    aula2 - antes

---
+++ Aula2/antes/ByteBank.Console/Program - original.cs
@@ -0,0 +1,100 @@
+﻿//internal class Program
+//{
+//    private static void Main(string[] args)
+//    {
+
+//        MostrarBanner();
+
+//        while (true)
+//        {
+//            MostrarMenu();
+
+//            if (int.TryParse(Console.ReadLine(), out int escolha))
+//            {
+//                ExecutarEscolha(escolha);
+//            }
+//            else
+//            {
+//                Console.WriteLine("Opção inválida. Tente novamente.");
+//            }
+//        }
+
+//    }
+//    static void MostrarBanner()
+//    {
+//        var bannerLines = new string[]
+//        {
+//            "▒█▀▀█ █░░█ ▀▀█▀▀ █▀▀ ▒█▀▀█ █▀▀█ █▀▀▄ █░█",
+//            "▒█▀▀▄ █▄▄█ ░░█░░ █▀▀ ▒█▀▀▄ █▄▄█ █░░█ █▀▄",
+//            "▒█▄▄█ ▄▄▄█ ░░▀░░ ▀▀▀ ▒█▄▄█ ▀░░▀ ▀░░▀ ▀░▀"
+//        };
+
+//        Console.ForegroundColor = ConsoleColor.DarkGreen;
+//        Console.WriteLine(bannerLines[0]);
+//        Console.ForegroundColor = ConsoleColor.White;
+//        Console.WriteLine(bannerLines[1]);
+//        Console.ForegroundColor = ConsoleColor.DarkYellow;
+//        Console.WriteLine(bannerLines[2]);
+//        Console.ForegroundColor = ConsoleColor.White;
+//    }
+
+//    static void MostrarMenu()
+//    {
+//        Console.WriteLine("\nEscolha uma opção:");
+//        Console.WriteLine();
+//        Console.WriteLine("1. Saldo");
+//        Console.WriteLine("2. Depositar valores");
+//        Console.WriteLine("3. Sacar valores");
+//        Console.WriteLine("4. Extrato");
+//        Console.WriteLine();
+//        Console.Write("Digite o número da opção desejada: ");
+//    }
+
+//    static void ExecutarEscolha(int escolha)
+//    {
+//        switch (escolha)
+//        {
+//            case 1:
+//                Saldo();
+//                break;
+//            case 2:
+//                Depositar();
+//                break;
+//            case 3:
+//                Sacar();
+//                break;
+//            case 4:
+//                Extrato();
+//                break;
+
+//            default:
+//                Console.WriteLine("Opção inválida. Tente novamente.");
+//                break;
+//        }
+//    }
+
+//    static CaixaEletronico caixaEletronico = new CaixaEletronico();
+
+//    private static void Saldo()
+//    {
+//        caixaEletronico.Saldo();
+//    }
+
+//    private static void Depositar()
+//    {
+//        caixaEletronico.Depositar(100);
+//        caixaEletronico.Depositar(40);
+//        caixaEletronico.Depositar(25);
+//    }
+
+//    private static void Sacar()
+//    {
+//        caixaEletronico.Sacar(50);
+//        caixaEletronico.Sacar(20);
+//    }
+
+//    private static void Extrato()
+//    {
+//        caixaEletronico.Extrato();
+//    }
+//}
