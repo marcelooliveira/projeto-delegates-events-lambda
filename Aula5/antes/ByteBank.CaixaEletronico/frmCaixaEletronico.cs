71acec768b40ec670de82d203c59f21573c4b12a
marcelooliveira <mclricardo@gmail.com>
2024-09-17 11:45:39 -0300

    Aula5 - antes - removendo referência para webApiClient e removendo lambda async

--- Aula5/antes/ByteBank.CaixaEletronico/frmCaixaEletronico.cs
+++ Aula5/antes/ByteBank.CaixaEletronico/frmCaixaEletronico.cs
@@ -5,18 +5,18 @@ namespace ByteBank.CaixaEletronico
 {
     public partial class frmCaixaEletronico : Form
     {
-        //private readonly CaixaEletronico caixaEletronico;
-        private readonly WebApiClient webApiClient;
-        private const string NumeroAgencia = "007";
-        private const string NumeroConta = "78901-2";
+        private readonly CaixaEletronico caixaEletronico;
+        //private readonly WebApiClient webApiClient;
+        //private const string NumeroAgencia = "007";
+        //private const string NumeroConta = "78901-2";
 
         public frmCaixaEletronico()
         {
             InitializeComponent();
-            //caixaEletronico = new CaixaEletronico();
-            webApiClient = new WebApiClient();
+            caixaEletronico = new CaixaEletronico();
+            //webApiClient = new WebApiClient();
 
-            //caixaEletronico.OnDeposito += CaixaEletronico_OnDeposito;
+            caixaEletronico.OnDeposito += CaixaEletronico_OnDeposito;
             //caixaEletronico.OnDeposito += (object sender, TransacaoEventArgs e) =>
             //caixaEletronico.OnDeposito += (sender, e) =>
             //{
@@ -25,7 +25,7 @@ namespace ByteBank.CaixaEletronico
             //    txtValor.Text = string.Empty;
             //};
 
-            //caixaEletronico.OnSaque += CaixaEletronico_OnSaque;
+            caixaEletronico.OnSaque += CaixaEletronico_OnSaque;
             //caixaEletronico.OnSaque += (object sender, TransacaoEventArgs e) =>
             //caixaEletronico.OnSaque += (sender, e) =>
             //{
@@ -34,7 +34,7 @@ namespace ByteBank.CaixaEletronico
             //    txtValor.Text = string.Empty;
             //};
 
-            //caixaEletronico.OnSaldoInsuficiente += CaixaEletronico_OnSaldoInsuficiente;
+            caixaEletronico.OnSaldoInsuficiente += CaixaEletronico_OnSaldoInsuficiente;
             //caixaEletronico.OnSaldoInsuficiente += (object sender, TransacaoEventArgs e) =>
             //caixaEletronico.OnSaldoInsuficiente += (sender, e) =>
             //{
@@ -43,51 +43,51 @@ namespace ByteBank.CaixaEletronico
 
             ImprimirLogo();
 
-            //btnSacar.Click += BtnSacar_Click;
-            btnSacar.Click += async (sender, e) =>
-            {
-                if (decimal.TryParse(txtValor.Text, out decimal valorSaque))
-                {
-                    var resposta = await webApiClient.SacarAsync(NumeroAgencia, NumeroConta, valorSaque);
-                    WriteToConsole(resposta);
-                    txtValor.Text = string.Empty;
-                }
-                else
-                {
-                    WriteToConsole("Valor inválido!");
-                    txtValor.Text = string.Empty;
-                }
-            };
+            btnSacar.Click += BtnSacar_Click;
+            //btnSacar.Click += async (sender, e) =>
+            //{
+            //    if (decimal.TryParse(txtValor.Text, out decimal valorSaque))
+            //    {
+            //        var resposta = await webApiClient.SacarAsync(NumeroAgencia, NumeroConta, valorSaque);
+            //        WriteToConsole(resposta);
+            //        txtValor.Text = string.Empty;
+            //    }
+            //    else
+            //    {
+            //        WriteToConsole("Valor inválido!");
+            //        txtValor.Text = string.Empty;
+            //    }
+            //};
 
-            //btnDepositar.Click += BtnDepositar_Click;
-            btnDepositar.Click += async (sender, e) =>
-            {
-                if (decimal.TryParse(txtValor.Text, out decimal valorDeposito))
-                {
-                    var resposta = await webApiClient.DepositarAsync(NumeroAgencia, NumeroConta, valorDeposito);
-                    WriteToConsole(resposta);
-                    txtValor.Text = string.Empty;
-                }
-                else
-                {
-                    WriteToConsole("Valor inválido!");
-                    txtValor.Text = string.Empty;
-                }
-            };
+            btnDepositar.Click += BtnDepositar_Click;
+            //btnDepositar.Click += async (sender, e) =>
+            //{
+            //    if (decimal.TryParse(txtValor.Text, out decimal valorDeposito))
+            //    {
+            //        var resposta = await webApiClient.DepositarAsync(NumeroAgencia, NumeroConta, valorDeposito);
+            //        WriteToConsole(resposta);
+            //        txtValor.Text = string.Empty;
+            //    }
+            //    else
+            //    {
+            //        WriteToConsole("Valor inválido!");
+            //        txtValor.Text = string.Empty;
+            //    }
+            //};
 
-            //btnSaldo.Click += BtnSaldo_Click;
-            btnSaldo.Click += async (sender, e) =>
-            {
-                decimal saldo = await webApiClient.GetSaldoAsync(NumeroAgencia, NumeroConta);
-                WriteToConsole($"Saldo atual: {saldo:C}");
-            };
+            btnSaldo.Click += BtnSaldo_Click;
+            //btnSaldo.Click += async (sender, e) =>
+            //{
+            //    decimal saldo = await webApiClient.GetSaldoAsync(NumeroAgencia, NumeroConta);
+            //    WriteToConsole($"Saldo atual: {saldo:C}");
+            //};
 
-            //btnExtrato.Click += BtnExtrato_Click;
-            btnExtrato.Click += async (sender, e) =>
-            {
-                string extrato = await webApiClient.GetExtratoAsync(NumeroAgencia, NumeroConta);
-                WriteToConsole(extrato);
-            };
+            btnExtrato.Click += BtnExtrato_Click;
+            //btnExtrato.Click += async (sender, e) =>
+            //{
+            //    string extrato = await webApiClient.GetExtratoAsync(NumeroAgencia, NumeroConta);
+            //    WriteToConsole(extrato);
+            //};
         }
 
         private void ImprimirLogo()
@@ -133,54 +133,55 @@ namespace ByteBank.CaixaEletronico
             txtValor.Text += btn.Name.Last();
         }
 
-        //private void BtnSacar_Click(object sender, EventArgs e)
-        //{
-        //    if (decimal.TryParse(txtValor.Text, out decimal valorSaque))
-        //    {
-        //        //caixaEletronico.Sacar(valorSaque);
+        private void BtnSacar_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtValor.Text, out decimal valorSaque))
+            {
+                caixaEletronico.Sacar(valorSaque);
 
-        //        var resposta = await webApiClient.SacarAsync(NumeroAgencia, NumeroConta, valorSaque);
-        //        WriteToConsole(resposta);
-        //        txtValor.Text = string.Empty;
-        //    }
-        //    else
-        //    {
-        //        WriteToConsole("Valor inválido!");
-        //        txtValor.Text = string.Empty;
-        //    }
-        //}
+                //var resposta = await webApiClient.SacarAsync(NumeroAgencia, NumeroConta, valorSaque);
+                //WriteToConsole(resposta);
+                txtValor.Text = string.Empty;
+            }
+            else
+            {
+                WriteToConsole("Valor inválido!");
+                txtValor.Text = string.Empty;
+            }
+        }
 
-        //private void BtnDepositar_Click(object sender, EventArgs e)
-        //{
-        //    if (decimal.TryParse(txtValor.Text, out decimal valorDeposito))
-        //    {
-        //        //caixaEletronico.Depositar(valorDeposito);
-        //        var resposta = await webApiClient.DepositarAsync(NumeroAgencia, NumeroConta, valorDeposito);
-        //        WriteToConsole(resposta);
-        //        txtValor.Text = string.Empty;
-        //    }
-        //    else
-        //    {
-        //        WriteToConsole("Valor inválido!");
-        //        txtValor.Text = string.Empty;
-        //    }
-        //}
+        private void BtnDepositar_Click(object sender, EventArgs e)
+        {
+            if (decimal.TryParse(txtValor.Text, out decimal valorDeposito))
+            {
+                caixaEletronico.Depositar(valorDeposito);
 
-        //private void BtnSaldo_Click(object sender, EventArgs e)
-        //{
-        //    //decimal saldo = caixaEletronico.Saldo;
-        //    decimal saldo = await webApiClient.GetSaldoAsync(NumeroAgencia, NumeroConta);
-        //    WriteToConsole($"Saldo atual: {saldo:C}");
-        //}
+                //var resposta = await webApiClient.DepositarAsync(NumeroAgencia, NumeroConta, valorDeposito);
+                //WriteToConsole(resposta);
+                txtValor.Text = string.Empty;
+            }
+            else
+            {
+                WriteToConsole("Valor inválido!");
+                txtValor.Text = string.Empty;
+            }
+        }
+
+        private void BtnSaldo_Click(object sender, EventArgs e)
+        {
+            decimal saldo = caixaEletronico.Saldo;
+            //decimal saldo = await webApiClient.GetSaldoAsync(NumeroAgencia, NumeroConta);
+            WriteToConsole($"Saldo atual: {saldo:C}");
+        }
 
 
-        //private async void BtnExtrato_Click(object? sender, EventArgs e)
-        //{
-        //    //string extrato = caixaEletronico.Extrato();
-        //    string extrato = await webApiClient.GetExtratoAsync(NumeroAgencia, NumeroConta);
+        private async void BtnExtrato_Click(object? sender, EventArgs e)
+        {
+            string extrato = caixaEletronico.Extrato();
+            //string extrato = await webApiClient.GetExtratoAsync(NumeroAgencia, NumeroConta);
 
-        //    WriteToConsole(extrato);
-        //}
+            WriteToConsole(extrato);
+        }
 
         private void btn_MouseDown(object sender, MouseEventArgs e)
         {
