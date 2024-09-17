8ee835f6be461be03d2a570addb119d664de4533
marcelooliveira <mclricardo@gmail.com>
2024-09-17 12:02:05 -0300

    Aula3 - antes - começando

---
+++ Aula3/antes/ByteBank.CaixaEletronico/ICaixaEletronico.cs
@@ -0,0 +1,12 @@
+﻿namespace ByteBank.CaixaEletronico
+{
+    public interface ICaixaEletronico
+    {
+        void Depositar(decimal valor);
+        void Sacar(decimal valor);
+        decimal Saldo();
+        string Extrato();
+
+        event SaldoInsuficienteEventHandler OnSaldoInsuficiente;
+    }
+}
